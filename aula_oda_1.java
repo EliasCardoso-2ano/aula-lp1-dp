320e8d99c809123d0a0db8b5b2c9048dce5504b0
EliasCardoso-2ano <elias.cardoso.da.silva.junior17@gmail.com>
2025-05-13 17:20:46 -0300

    e

---
+++ aula_oda_1.java
@@ -0,0 +1,14 @@
+public class aula_oda_1 {
+
+    public static void main(String[] args) {
+        byte saldo = 0;
+        short andares = 0;
+        imt aluno = 0;
+        long populacao = 0;
+        float tempeatura = 0f;//coloca o f no final para formatar 
+        double = tamTransistor = 0;
+        boolean arCondicionadoLigado = false;//primeiramente coloca o false
+        char turma = ''
+
+    }
+} 
---
+++ sla.java
@@ -0,0 +1,36 @@
+public class sla {
+    public static void main(String[] args) {
+        float area = 0f;
+        float aultura = 0f;
+        float base = 0f;
+
+        imprimirTexto(texto: "Digite o valor da altura: ")
+
+        public static void imprimirTexto(String texto)
+
+
+
+
+
+
+          area = (aultura*base)/2;
+          
+          
+
+
+
+
+
+
+    }
+    public static float cauculadorAreaTriangulo(float altura , float base)
+    }
+
+
+
+
+
+
+    
+    psv
+}
