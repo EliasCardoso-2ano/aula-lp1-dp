bc8f66536309cf9cfd5620f4c640907414fedafa
EliasCardoso-2ano <elias.cardoso.da.silva.junior17@gmail.com>
2025-05-15 18:36:20 -0300

    sla.java

--- sla.java
+++ sla.java
@@ -1,36 +1,48 @@
+import java.util.Scanner;
+
 public class sla {
+
+    final static Scanner LER = new Scanner(System.in);
+
     public static void main(String[] args) {
         float area = 0f;
         float aultura = 0f;
         float base = 0f;
 
-        imprimirTexto(texto: "Digite o valor da altura: ")
+        imprimirTexto("Digite a altura:");
+        aultura = lerNumeroFloat();
 
-        public static void imprimirTexto(String texto)
+        imprimirTexto("digite a base:");
+        base = lerNumeroFloat();
+        
+        imprimirTexto("calculando área.........");
+        area = calcularAreaTriangulo(base, aultura);
 
-
-
-
-
-
-          area = (aultura*base)/2;
-          
-          
-
-
-
+        imprimirTexto("Área do Triangulo é: " + area + "m²");
 
 
 
     }
-    public static float cauculadorAreaTriangulo(float altura , float base)
+
+    public static void imprimirTexto(String texto){
+        System.out.println(texto);
+    }
+
+    public static float lerNumeroFloat(){
+        float n = 0f;
+        n = LER.nextFloat();
+        return n;
+    }
+
+    public static float calcularAreaTriangulo(float b, float a){
+
+        float area = 0f;
+
+        area = (b * a) / 2;
+
+        return area;
     }
 
 
 
-
-
-
-    
-    psv
 }
